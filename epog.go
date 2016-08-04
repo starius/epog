927722714c28978d459d285749968066f2ec7daa
Boris Nagaev <bnagaev@gmail.com>
2016-08-05 01:36:01 +0300

    implement fixed target tcp proxy -> socks5 proxy

--- epog.go
+++ epog.go
@@ -4,9 +4,12 @@ import (
     "log"
     "net"
     "net/url"
+    "time"
 
     "golang.org/x/net/proxy"
 )
+
+const BUFFER_SIZE = 1024
 
 func connectToProxy(proxyUrl, targetServer string) (net.Conn, error) {
     parsedUrl, err := url.Parse(proxyUrl)
@@ -21,10 +24,32 @@ func connectToProxy(proxyUrl, targetServer string) (net.Conn, error) {
     return connection, err
 }
 
-func main() {
+func netCopy(from, to net.Conn, finished chan<- struct{}) {
+    defer func() {
+        finished<-struct{}{}
+    }()
+    buffer := make([]byte, BUFFER_SIZE)
+    for {
+        from.SetReadDeadline(time.Now().Add(time.Duration(10e9)))
+        bytesRead, err := from.Read(buffer)
+        if err != nil {
+            log.Printf("Finished reading: %s", err)
+            break
+        }
+        to.SetWriteDeadline(time.Now().Add(time.Duration(10e9)))
+        _, err = to.Write(buffer[:bytesRead])
+        if err != nil {
+            log.Printf("Finished writting: %s", err)
+            break
+        }
+    }
+}
+
+func processRequest(clientConn net.Conn) {
+    defer clientConn.Close()
     proxyUrl := "socks5://127.0.0.1:9150"
-    targetServer := "ip4.me:80"
-    _, err := connectToProxy(proxyUrl, targetServer)
+    targetServer := "redjohn.tk:4218"
+    serverConn, err := connectToProxy(proxyUrl, targetServer)
     if err != nil {
         log.Printf(
             "Unable to connect to %s through %s: %s\n",
@@ -33,5 +58,27 @@ func main() {
             err,
         )
         return
+    }
+    defer serverConn.Close()
+    finished := make(chan struct{})
+    go netCopy(clientConn, serverConn, finished)
+    go netCopy(serverConn, clientConn, finished)
+    <-finished
+    <-finished
+}
+
+func main() {
+    listenOn := "127.0.0.1:4218"
+    listener, err := net.Listen("tcp", listenOn)
+    if err != nil {
+        log.Fatal("Unable to listen on %s: %s", listenOn, err)
+    }
+    for {
+        conn, err := listener.Accept()
+        if err == nil {
+            go processRequest(conn)
+        } else {
+            log.Printf("Unable to accept request: %s", err)
+        }
     }
 }
